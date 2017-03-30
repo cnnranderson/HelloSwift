909680f2307e2a41e0bdd4a4125dcd8fb108f57a
Conner Anderson <cnnranderson@gmail.com>
2017-03-29 20:53:52 -0700

    Initial Commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Conner Anderson on 3/29/17.
+//  Copyright © 2017 Conner Anderson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
